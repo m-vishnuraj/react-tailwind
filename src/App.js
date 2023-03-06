dad2259b1ad60146d5d0145044e2e6a33c2264f4
m-vishnuraj <mvishnurajm@gmail.com>
2023-03-06 17:24:12 +0530

    flex wrap completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 function App() {
   return (
     <div>
-      <div className="flex bg-yellow-500 mx-auto">
-        <div className="w-10 h-10 bg-red-500">1</div>
-        <div className="w-10 h-10 bg-green-500">2</div>
-        <div className="w-10 h-10 bg-blue-500">3</div>
+      <div className="flex flex-wrap bg-yellow-500 mx-auto">
+        <div className="w-80 h-10 bg-red-500">1</div>
+        <div className="w-80 h-10 bg-green-500">2</div>
+        <div className="w-80 h-10 bg-blue-500">3</div>
       </div>
-      <div className="flex flex-col bg-violet-400 mx-auto">
-        <div className="w-10 h-10 bg-red-500">1</div>
-        <div className="w-10 h-10 bg-green-500">2</div>
-        <div className="w-10 h-10 bg-blue-500">3</div>
-      </div>
-      <div className="flex flex-row-reverse bg-orange-400 mx-auto">
-        <div className="w-10 h-10 bg-red-500">1</div>
-        <div className="w-10 h-10 bg-green-500">2</div>
-        <div className="w-10 h-10 bg-blue-500">3</div>
-      </div>
-      <div className="flex flex-col-reverse bg-yellow-400 mx-auto">
-        <div className="w-10 h-10 bg-red-500">1</div>
-        <div className="w-10 h-10 bg-green-500">2</div>
-        <div className="w-10 h-10 bg-blue-500">3</div>
+      <hr />
+      <div className="flex flex-wrap-reverse bg-yellow-500 mx-auto">
+        <div className="w-80 h-10 bg-red-500">1</div>
+        <div className="w-80 h-10 bg-green-500">2</div>
+        <div className="w-80 h-10 bg-blue-500">3</div>
       </div>
     </div>
   )
