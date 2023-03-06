55adf6bc5a28d34d7fa083ae75deffe919ae7522
m-vishnuraj <mvishnurajm@gmail.com>
2023-03-07 04:44:33 +0530

    Using Breakpoints To Add Responsive Behavior

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div>
-      <button className="bg-blue-500 m-10 p-2 rounded border-4 text-white shadow-2xl hover:bg-gray-600">Click here</button>
+      <h1 className="text-5xl text-pink-500 2xl:text-blue-500 xl:text-green-500 lg:text-yellow-500 md:text-red-500 sm:text-amber-500">Heading</h1>
+      <h1 className="  2xl:text-4xl xl:text-2xl lg:text-xl md:text-lg sm:text-sm">Heading</h1>
     </div>
   )
 }
