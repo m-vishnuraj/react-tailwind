107173d41ae55f1fe4c442c1b560bf57e124392d
m-vishnuraj <mvishnurajm@gmail.com>
2023-03-06 17:08:58 +0530

    flex box completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 function App() {
   return (
-    <div className="w-40 h-40 bg-red-500 ">
-      this is a div element
+    <div>
+      <div className="flex bg-yellow-500 mx-auto">
+        <div className="w-10 h-10 bg-red-500">1</div>
+        <div className="w-10 h-10 bg-green-500">2</div>
+        <div className="w-10 h-10 bg-blue-500">3</div>
+      </div>
+      <div className="flex flex-col bg-violet-400 mx-auto">
+        <div className="w-10 h-10 bg-red-500">1</div>
+        <div className="w-10 h-10 bg-green-500">2</div>
+        <div className="w-10 h-10 bg-blue-500">3</div>
+      </div>
+      <div className="flex flex-row-reverse bg-orange-400 mx-auto">
+        <div className="w-10 h-10 bg-red-500">1</div>
+        <div className="w-10 h-10 bg-green-500">2</div>
+        <div className="w-10 h-10 bg-blue-500">3</div>
+      </div>
+      <div className="flex flex-col-reverse bg-yellow-400 mx-auto">
+        <div className="w-10 h-10 bg-red-500">1</div>
+        <div className="w-10 h-10 bg-green-500">2</div>
+        <div className="w-10 h-10 bg-blue-500">3</div>
+      </div>
     </div>
   )
 }
