35af352ef3962aa2fbfc35c7c2ff6c5a5b3413f9
m-vishnuraj <mvishnurajm@gmail.com>
2023-03-06 20:09:59 +0530

    Adding Padding & Margin To Elements

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 function App() {
   return (
-    <div className="bg-orange-400 grid grid-cols-5 grid-flow-row  gap-x-3 gap-y-5 ">
-      <div className="h-20 w-20 bg-red-400 justify-self-end">1</div>
-      <div className="h-20 w-20 bg-green-400">2</div>
-      <div className="h-20 w-20 bg-blue-400">3</div>
-      <div className="h-20 w-20 bg-purple-400">4</div>
-      <div className="h-20  bg-pink-400 justify-self-stretch">5</div>
-      <div className="h-20 w-20 bg-yellow-400">6</div>
-      <div className="h-20 w-20 bg-red-400">7</div>
-      <div className="h-20 w-20 bg-green-400">8</div>
-      <div className="h-20 w-20 bg-blue-400">9</div>
+    <div className="bg-green-300 m-5 p-2">
+      <div className="bg-red-300">this is an inner div</div>
     </div>
   )
 }
