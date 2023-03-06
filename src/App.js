5f445cad2ef3607b1b78449c4caa4d145561feb1
m-vishnuraj <mvishnurajm@gmail.com>
2023-03-06 23:13:03 +0530

    Adding Shadows To Elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <button className="bg-blue-500 m-10 p-2 rounded border-4 border-red-500 opacity-50 border-dashed hover:bg-gray-600">Click here</button>
+      <button className="bg-blue-500 m-10 p-2 rounded border-4 text-white shadow-2xl hover:bg-gray-600">Click here</button>
     </div>
   )
 }
